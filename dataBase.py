864c98872ba0779c047c1ca46c12548923b93e71
Vanessa Reteguín <145515264+vcardui@users.noreply.github.com>
2024-09-06 20:44:08 -0600

    Update dataBase.py

--- dataBase.py
+++ dataBase.py
@@ -8,7 +8,7 @@
 # +----------------------------------------------------------------------------+
 # | Author.......: Vanessa Reteguín <vanessa@reteguin.com>
 # | First release: July 9th, 2024
-# | Last update..: July 9th, 2024
+# | Last update..: July 20th, 2024
 # | WhatIs.......: InstagramBot_dataBase - Class
 # +----------------------------------------------------------------------------+
 
@@ -19,6 +19,8 @@
 # Pandas read csv() Tutorial: https://www.datacamp.com/tutorial/pandas-read-csv
 # How do I read and write tabular data?:
 #     https://pandas.pydata.org/docs/getting_started/intro_tutorials/02_read_write.html
+# NEVER grow a DataFrame row-wise!
+#   https://stackoverflow.com/questions/13784192/creating-an-empty-pandas-dataframe-and-then-filling-it
 
 # ------------------------- Libraries -------------------------
 import datetime  # datetime.datetime.now()
@@ -40,12 +42,30 @@ class InstagramPost:
 
     def __init__(self):
         self.path = instagramPost_path
-        self.dataBase = None
+        self.DataFrame = None
 
     def get_data_base(self):
         if os.path.exists(self.path):
             print("File exists")
-            self.dataBase = pd.read_csv(self.path, index_col="idinstagrampost")
-            self.dataBase.head()
+            self.DataFrame = pd.read_csv(self.path, index_col="idinstagrampost")
+            self.DataFrame.head()
         else:
-            print("404")
+            print("Creating new DataFrame...")
+            self.DataFrame = pd.DataFrame(
+                columns=['idinstagrampost', 'caption', 'pathtoimage', 'dateadded', 'dateposted'])
+            self.DataFrame.to_csv(path_or_buf="instagrampost.csv", index=False)
+            print("👍New DataFrame successfully created")
+
+    def new_row(self, postCaption, pathtoimage):
+        instagram_post_dic = []
+        row = {
+            "idinstagrampost": 1,
+            "caption": postCaption,
+            "pathtoimage": pathtoimage,
+            "dateadded": nowDateTime,
+            "dateposted": ""
+        }
+        instagram_post_dic.append(row)
+        dic = pd.DataFrame(instagram_post_dic)
+        self.DataFrame = pd.concat([self.DataFrame, dic])
+        print("👍New row successfully added")
